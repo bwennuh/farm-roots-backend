8bcb98de918a962681e2e3e6ed7e8ca043fafe79
bwennuh <brennabruening@gmail.com>
2021-06-24 16:05:19 -0400

    Add validations to products and farms

--- app/models/farm.rb
+++ app/models/farm.rb
@@ -2,4 +2,9 @@ class Farm < ApplicationRecord
 
     has_many :products
 
+    validates :name, presence: true
+    validates :bio, presence: true
+    validates :address, presence: true
+    validates :farmer_name, presence: true
+
 end
